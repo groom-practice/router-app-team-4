93af5f343d4697ef90716a2f495e5be85ff83d91
daj3on9 <dajung5623@naver.com>
2025-04-21 11:16:02 +0900

    Feact : 홈 화면 구현

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+import "./index.css";
 
 export default function Home() {
-  return <h3>Welcome React Router CRUD App!</h3>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="home">
+      <h3>Welcome To Main Page!</h3>
+      <button onClick={() => setShowModal(true)}>로그인</button>
+      {showModal &&
+        createPortal(
+          <LoginModal onClose={() => setShowModal(false)} />,
+          document.body
+        )}
+    </div>
+  );
+}
+
+// 로그인 모달
+function LoginModal({ onClose }) {
+  return (
+    <div className="login-modal">
+      <div className="login-modal-content">
+        <p> 로그인 해주세요 </p>
+        <input placeholder="id : test123" />
+        <input placeholder="pw : test123password" />
+        <div className="login-modal-button">
+          <button>로그인</button>
+          <button onClick={onClose}>취소</button>
+        </div>
+      </div>
+    </div>
+  );
 }
